3755ff3fa10fb2d2c525ce7f6978de09bc55def3
aivan <haivan.villegas@gmail.com>
2022-04-24 16:25:55 -0500

    added products and reviews mutations

--- products/products.model.js
+++ products/products.model.js
@@ -3,11 +3,13 @@ const products = [
     id: "rs01",
     description: "red shoe",
     price: 24.5,
+    reviews: [],
   },
   {
     id: "blj01",
     description: "Blue Jean",
     price: 12.5,
+    reviews: [],
   },
 ];
 
@@ -27,8 +29,37 @@ function getProductById(id) {
   });
 }
 
+function addNewProduct(id, description, price) {
+  const newProduct = {
+    id,
+    price,
+    description,
+    reviews: [],
+  };
+  products.push(newProduct);
+
+  return newProduct;
+}
+
+function addNewReview(id, rating, comment) {
+  const productReviewed = getProductById(id);
+
+  if (productReviewed) {
+    const NewReview = {
+      rating,
+      comment,
+    };
+    productReviewed.reviews.push(NewReview);
+    return NewReview;
+  }
+
+  return null;
+}
+
 module.exports = {
   getAllProducts,
   getProductsByPrice,
   getProductById,
+  addNewProduct,
+  addNewReview,
 };
