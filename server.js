4fc8e13167e4989f749997bbb148ed43e85f7117
aivan <haivan.villegas@gmail.com>
2022-04-24 16:46:08 -0500

    added apollo to the server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
+const { ApolloServer } = require("apollo-server-express");
+
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const { loadFilesSync } = require("@graphql-tools/load-files");
 const path = require("path");
@@ -7,21 +8,24 @@ const path = require("path");
 const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
 const resolversArray = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
 
-const schema = makeExecutableSchema({
-  typeDefs: typesArray,
-  resolvers: resolversArray,
-});
+async function startApolloServer() {
+  const app = express();
+  const schema = makeExecutableSchema({
+    typeDefs: typesArray,
+    resolvers: resolversArray,
+  });
 
-const app = express();
-
-app.use(
-  "/graphql",
-  graphqlHTTP({
+  const server = new ApolloServer({
     schema,
-    graphiql: true,
-  })
-);
+  });
 
-app.listen(3000, () => {
-  console.log("Running GraphQL server");
-});
+  await server.start();
+
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  app.listen(3000, () => {
+    console.log("Running GraphQL server");
+  });
+}
+
+startApolloServer();
